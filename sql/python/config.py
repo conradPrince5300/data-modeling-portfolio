cc7ed7963a127729148d61044238868abe8b59f3
conradPrince5300 <conradoomadi@gmail.com>
2025-11-05 13:58:43 +0000

    Create config.py

---
+++ sql/python/config.py
@@ -0,0 +1,8 @@
+# Database connection configuration
+DB_CONFIG = {
+    'dbname': 'data_modeling',
+    'user': 'postgres',
+    'password': 'yourpassword',
+    'host': 'localhost',
+    'port': '5432'
+}
